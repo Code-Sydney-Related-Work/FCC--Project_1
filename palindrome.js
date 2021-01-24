3e7611c2f44ace1fb144127ccfe8be56484276a1
UtpalB-arch <67811773+UtpalB-arch@users.noreply.github.com>
2021-01-25 06:13:20 +1100

    Update palindrome.js

--- palindrome.js
+++ palindrome.js
@@ -2,7 +2,7 @@ function palindromecheck(str)
         {
             let ourStr = arguments[0].toLowerCase();  //Converting all Upper case letters to small
             console.log("The string being tested is " + ourStr);
-            let ourRegex = /[A-Za-z0-9]/ig;   //[^A-Za-z0-9] or \W regex for matching non alphanumeric characters
+            let ourRegex = XXXXXXXXXXXXX   PORTION OF CODE DELETED TO PROTECT INTELLECTUAL PROPERTY  XXXXXXXXXXXXXX
             let AlphaNumArray = ourStr.match(ourRegex); // An array of all non-alphanumeric characters present
             //console.log("The NonAlphaNumArray is " + NonAlphaNumArray);
             console.log(AlphaNumArray);
