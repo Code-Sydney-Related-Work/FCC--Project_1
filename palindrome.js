59a6252cad4f097913750819ca5c24afe345a936
UtpalB-arch <67811773+UtpalB-arch@users.noreply.github.com>
2021-01-25 06:12:36 +1100

    Update palindrome.js

--- palindrome.js
+++ palindrome.js
@@ -9,28 +9,7 @@ function palindromecheck(str)
            
             let palinFlag = false;
             let limit;
-            //console.log("The length of the Array is " + AlphaNumArray.length);
-            if(AlphaNumArray.length % 2 == 0) // Code for checking potential palindrome - Array length is an even number
-            {
-                limit = AlphaNumArray.length/2;
-                for(let j = 0; j < limit; j++)
-                {
-                    //console.log(AlphaNumArray[j] + "," + AlphaNumArray[2*limit - 1 - j]);
-                    if(AlphaNumArray[j] === AlphaNumArray[2*limit - 1 - j]){palinFlag = true; }
-                    else{palinFlag = false; break;}
-                }
-            }
-                else if(AlphaNumArray.length % 2 != 0) // Code for checking potential palindrome - Array length is an odd number
-            {
-                limit = Math.round(AlphaNumArray.length/2);
-                limit = (limit) - 1 ;
-                //console.log(limit);
-                for(let j = 0; j < limit; j++)
-                {   
-                    //console.log(AlphaNumArray[j] + "," + AlphaNumArray[2*limit - j]);
-                    if(AlphaNumArray[j] === AlphaNumArray[2*limit - j]){palinFlag = true; ;}//console.log(palinFlag)
-                    else{palinFlag = false; break;}
-                }
+            XXXXXXXXXXXXXXXXXX  PORTION OF CODE DELETED TO PROTECT INTELLECTUAL PROPERTY  XXXXXXXXXXXXXXXX
             } return palinFlag;
         } 
         module.exports = palindromecheck;           
